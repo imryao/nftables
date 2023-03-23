803f8059435bf996f084228000c0df66d90e81bb
Pablo Neira Ayuso <pablo@netfilter.org>
2023-03-23 12:36:08 +0100

    evaluate: set up integer type to shift expression
    commit 1cdd8249f1c9c462b40ccde8d56191ea481ce610 upstream.
    
    Otherwise expr_evaluate_value() fails with invalid datatype:
    
     # nft --debug=netlink add rule ip x y 'ct mark set ip dscp & 0x0f << 1'
     BUG: invalid basetype invalid
     nft: evaluate.c:440: expr_evaluate_value: Assertion `0' failed.
    
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- src/evaluate.c
+++ src/evaluate.c
@@ -1213,6 +1213,7 @@ static int expr_evaluate_shift(struct eval_ctx *ctx, struct expr **expr)
 	if (byteorder_conversion(ctx, &op->right, BYTEORDER_HOST_ENDIAN) < 0)
 		return -1;
 
+	datatype_set(op, &integer_type);
 	op->byteorder = BYTEORDER_HOST_ENDIAN;
 	op->len	      = max_shift_len;
 
